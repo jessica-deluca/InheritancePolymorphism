45eaad4f463776467192ee2d2aa60633d1876547
Your Name <you@example.com>
2017-07-13 14:31:12 -0400

    added abstract class

---
+++ InheritancePolymorphism/Appliances.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphism
+{
+    abstract class Appliances
+    {
+        public abstract void Clean(); // don't have to include virtual because there is nothing to inherit
+
+
+    }
+}
